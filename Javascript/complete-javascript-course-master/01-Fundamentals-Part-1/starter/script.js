ecf24f3512303f0e2bdab7ac4a2477df07b224d5
fightforhash <rackoon1030@gmail.com>
2024-12-11 01:21:12 -0800

    12/11

---
+++ Javascript/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,106 @@
+let js = 'amazing';
+if (js === 'amazing') alert('JavaScript is FUN!');
+
+40 + 8 + 23 - 10;
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Bob";
+let PI = 3.1415; 
+
+console.log(firstName);
+
+// 그리고 아는것들 반복 건너뜀.
+
+//Assignment
+
+let country = 50;
+let continent = 30;
+let population = 2000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+true;
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof(true));
+
+//Truthy and falsy values
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const favorite = prompt("Whats your favorite number?");
+console.log(favorite)
+console.log(typeof favorite);
+
+if (favorite == 23){
+    console.log("Cool! 23 is an amazing number!");
+} else if (favorite === 7){
+    console.log('7 is also a cool number');
+} else if (favorite === 9){
+    console.log('9 is also cool number')
+}else{
+    console.log("Number is not 23 or 7 or 9");
+}
+
+if (favorite !== 23) console.log("Why not 23?");
+
+// Boolean logic
+
+const hasDriversLicense = true;
+const hasGoodVision = false;
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+const shouldDrive = hasDriversLicense && hasGoodVision && !isTired;;
+
+/*
+if(shouldDrive){
+    console.log("Sarah is able to Drive!");
+}else{
+    console.log("Someone else should drive...");
+}
+*/
+
+const isTired = true; // C
+console.log(hasDriversLicense && hasGoodVision  && isTired);
+
+if(shouldDrive){
+    console.log("Sarah is able to Drive!");
+}else{
+    console.log("Someone else should drive...");
+}
+
+//Coding Challenge 3
+//파일로해결
+
+//switch statements
+
+const day = 'wednesday';
+
+switch(day){
+    case 'monday': // day === 'monday'
+        console.log("Plan course structure");
+        console.log("Go to coding meetup");
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log("Write code examples");
+        break;
+    default:
+        console.log("Not a valid day!");
+}
