e2c67de69bb1d97c0a36dae832c6a9dfdd346b3d
T <78007693+fightforhash@users.noreply.github.com>
2025-01-15 06:58:02 -0800

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    
+    int ans = 0;
+
+    for (int i = 0; i < n; i++){
+        for (int j = i + 1; j < n; j++){
+            for (int k = j + 1; k < n; k++){
+                int tmp = arr[i] + arr[j] + arr[k];
+                if (tmp > m){
+                    continue;
+                }
+                else if (tmp < ans){
+                    continue;
+                }
+                else{
+                    ans = tmp;
+                }
+
+            }
+        }
+    }
+
+    cout << ans;
+
+}
