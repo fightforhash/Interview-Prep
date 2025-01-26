f8bb957ce60965b60d0889a8856520dd9f6cd4e2
T <78007693+fightforhash@users.noreply.github.com>
2025-01-25 20:05:35 -0800

    [Silver IV] Title: solved.ac, Time: 28 ms, Memory: 3192 KB -BaekjoonHub

---
+++ 백준/Silver/18110. solved．ac/solved．ac.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    cin.tie(0) -> sync_with_stdio(0);
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for (int i =0; i < n; i ++){
+
+        cin >> arr[i];
+    }
+
+    sort(arr.begin(), arr.end());
+    if (n == 0){
+        cout << 0 << '\n';
+        return 0;
+    }
+    int bottom = (15 * n + 50) / 100;
+    int top = n - 1 - ((15 * n + 50) / 100);
+
+    int difficulty = 0;
+    for (int i = bottom; i <= top; i++ ){
+        difficulty += arr[i];
+    }
+
+    int ans = ((10 * difficulty) + (5 * (top - bottom + 1))) / (10 * (top - bottom + 1));
+
+    cout << ans;
+
+}
